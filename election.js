41fe75bb5ef6546eb860cbf7014d408e5eb995f4
eiji <eijimine@gmail.com>
2017-08-22 16:24:05 -0400

    Part 1 complete

--- election.js
+++ election.js
@@ -1,5 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+var candidates = document.getElementById('candidates');
+
+  $.ajax({
+    url:  'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(data) {
+      data.candidates.forEach(function(d) {
+        var li = document.createElement('li');
+        var name = 'Name: '
+        var vote = ' Votes: '
+          li.append(name);
+          li.append(d.name);
+          li.append(vote);
+          li.append(d.votes);
+          candidates.append(li);
+      });
+  });
+
+
 
 });
